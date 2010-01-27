d6718beae0343bfa55f70caa6fada6451ef37d96
rdp <rogerpack2005@gmail.com>
2010-01-26 17:08:21 -0700

    output what is being run

--- lib/spork/run_strategy/single_process_exits.rb
+++ lib/spork/run_strategy/single_process_exits.rb
@@ -1,8 +1,8 @@
 require File.dirname(__FILE__) + '/single_process_looping.rb'
-require 'ruby-prof'
 class Spork::RunStrategy::SingleProcessExits < Spork::RunStrategy::SingleProcessLooping
   
     def run(argv, stderr, stdout)
+      puts "SingleProcessExits running " + argv.inspect
       super(argv, stderr, stdout)
       Thread.main.raise 'exit please' # seems to be the only way to exit easily...
     end
