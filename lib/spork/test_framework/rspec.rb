d4717d0f8f9762fdc7f21fe438b5346b1a725c8b
Nick Sieger <nick@nicksieger.com>
2010-06-03 22:05:55 -0500

    Add RSpec 2 support

--- lib/spork/test_framework/rspec.rb
+++ lib/spork/test_framework/rspec.rb
@@ -3,12 +3,26 @@ class Spork::TestFramework::RSpec < Spork::TestFramework
   HELPER_FILE = File.join(Dir.pwd, "spec/spec_helper.rb")
 
   def run_tests(argv, stderr, stdout)
-    ::Spec::Runner::CommandLine.run(
-      ::Spec::Runner::OptionParser.parse(
-        argv,
-        stderr,
-        stdout
+    if defined?(::RSpec)    # RSpec 2
+      begin
+        prevout, preverr, $stdout, $stderr = $stdout, $stderr, stdout, stderr
+        runner = ::RSpec::Core::Runner
+        def runner.installed_at_exit?; true; end # no autorun
+        ::RSpec::Core::Runner.new.run(argv)
+      ensure
+        ::RSpec.world.example_groups.clear
+        ::RSpec.world.filtered_examples.clear
+        ::RSpec.world.shared_example_groups.clear
+        $stdout, $stderr = prevout, preverr
+      end
+    else
+      ::Spec::Runner::CommandLine.run(
+        ::Spec::Runner::OptionParser.parse(
+          argv,
+          stderr,
+          stdout
+        )
       )
-    )
+    end
   end
 end
